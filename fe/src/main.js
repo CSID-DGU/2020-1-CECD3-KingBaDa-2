fcb5096ed05e4655a1b2f5b6806042b05935ca4b
moongchi95 <44993287+moongchi95@users.noreply.github.com>
2020-07-29 15:32:57 +0900

    sidebar
    sidebar

--- fe/src/main.js
+++ fe/src/main.js
@@ -1,14 +1,17 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import VueSidebarMenu from 'vue-sidebar-menu'
-import 'vue-sidebar-menu/dist/vue-sidebar-menu.css'
+import "@babel/polyfill";
+import "mutationobserver-shim";
+import Vue from "vue";
+import "./plugins/bootstrap-vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import VueSidebarMenu from "vue-sidebar-menu";
+import "vue-sidebar-menu/dist/vue-sidebar-menu.css";
 
-Vue.config.productionTip = false
-Vue.use(VueSidebarMenu)
+Vue.config.productionTip = false;
+Vue.use(VueSidebarMenu);
 new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
--- fe/src/router/index.js
+++ fe/src/router/index.js
@@ -1,34 +1,35 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Main from '../views/Main.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Main from "../views/Main.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
   {
-    path: '/main',
-    name: 'Main',
+    path: "/main",
+    name: "Main",
     component: Main
   },
   {
-    path: '/about',
-    name: 'About',
+    path: "/about",
+    name: "About",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/About.vue")
   },
   {
-    path: '/',
-    name: 'Login',
-    component: () => import('../views/Login.vue')
+    path: "/",
+    name: "Login",
+    component: () => import("../views/Login.vue")
   }
-]
+];
 
 const router = new VueRouter({
-  mode: 'history',
+  mode: "history",
   base: process.env.BASE_URL,
   routes
-})
+});
 
-export default router
+export default router;
