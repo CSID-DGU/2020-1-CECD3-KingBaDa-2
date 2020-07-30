e20088995b2b95055c01a9fef1516e789484c490
insolk <rkddlsthf123@naver.com>
2020-07-30 17:39:43 +0900

    add graph

--- be/fe/src/views/Main.vue
+++ be/fe/src/views/Main.vue
@@ -1,66 +1,71 @@
 <template>
-  <div class = "main">
-    <div class = "backimg">
-      <img src = "../assets/backg.png" />
-      <div class = "title">
+  <div class="main">
+    <div class="backimg">
+      <img src="../assets/backg.png" />
+      <div class="title">
         <h1>DIS</h1>
-        <h3>Dashboard Integration System</h3></div>
-        <br><br><br>
+        <h3>Dashboard Integration System</h3>
+      </div>
+      <br />
+      <br />
+      <br />
     </div>
     <div>
-      <b-button
-      pill variant="info"
-      size = "lg"
-      @click="$router.push('Dashboard')">
-        <img
-        class = "icon-img"
-        src = "../assets/icon/dashboard.png"
-        /><br>
-        대시보드 조회</b-button><br><br>
+      <b-button pill variant="info" size="lg" @click="$router.push('Dashboard')">
+        <img class="icon-img" src="../assets/icon/dashboard.png" />
+        <br />대시보드 조회
+      </b-button>
+      <br />
+      <br />
     </div>
     <div>
-      <router-link to="/Notice">공지사항</router-link><br><hr>
-      {{ 공지사항1 }}<br>
-      {{ 공지사항2 }}<br>
-      {{ 공지사항3 }}<br>
+      <router-link to="/id/notice">공지사항</router-link>
+      <br />
+      <hr />
+      {{ 공지사항1 }}
+      <br />
+      {{ 공지사항2 }}
+      <br />
+      {{ 공지사항3 }}
+      <br />
     </div>
-
   </div>
 </template>
 
 <script>
 export default {
-data () {
+  data() {
     return {
-      공지사항1: '공지 사항1',
-      공지사항2: '공지 사항2',
-      공지사항3: '공지 사항3'
-    }
+      공지사항1: "공지 사항1",
+      공지사항2: "공지 사항2",
+      공지사항3: "공지 사항3"
+    };
   }
-}
+};
 </script>
 
 <style>
-  .backimg{
-    position: relative;
-    background-size: cover;
-  }
-  .backimg .title{
-    position: absolute;
-     top:50%;
-     left:50%;
-     transform: translate(-50%, -50%);
-     font-size:5rem;
-     color: white;
-     z-index: 2;
-     text-align: center;
-  }
-  .icon-img{
-        width: auto; height: auto;
-    max-width: 100px;
-    max-height: 100px;
-  }
-  hr{
-    width:10%
-  }
+.backimg {
+  position: relative;
+  background-size: cover;
+}
+.backimg .title {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  font-size: 5rem;
+  color: white;
+  z-index: 2;
+  text-align: center;
+}
+.icon-img {
+  width: auto;
+  height: auto;
+  max-width: 100px;
+  max-height: 100px;
+}
+hr {
+  width: 10%;
+}
 </style>
