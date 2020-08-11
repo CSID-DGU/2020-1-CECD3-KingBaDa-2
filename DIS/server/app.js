15789ef3c44be6035e0d48ad521b3e1ab5e895d6
biny <kbpark829@gmail.com>
2020-08-11 14:01:01 +0900

    add login api

--- DIS/server/app.js
+++ DIS/server/app.js
@@ -1,24 +1,19 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const history = require('connect-history-api-fallback');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
+let app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static(path.join(__dirname, 'fe', 'dist')));
+app.use('/api', require('./routes/api'));
+app.use(history());
+app.use(express.static(path.join(__dirname, '../', 'build')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
@@ -36,7 +31,10 @@ app.use(function(err, req, res, next) {
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.send({ msg: err.message });
+  console.error(err.message);
 });
+
+const cfg = require('');
 
 module.exports = app;
---
+++ DIS/server/routes/api/sign/index.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const createError = require('http-errors');
+const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+const request = require('request');
+
+router.post('/signin', (req, res, next) => {
+    
+})
