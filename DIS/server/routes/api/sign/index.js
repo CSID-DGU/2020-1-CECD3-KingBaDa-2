548b446584ac7ad16a64eaadbe63860506a290b8
dss1111 <dss11111@naver.com>
2020-08-12 10:24:35 +0900

    프론트에서 로그인 json으로 넘기도록 수정, 백엔드 받는 부분 구현

--- DIS/server/routes/api/sign/index.js
+++ DIS/server/routes/api/sign/index.js
@@ -5,6 +5,7 @@ const crypto = require('crypto');
 const request = require('request');
 
 router.post('/login', (req, res, next) => {
+    console.log(req.body.data)
     res.send({ success: true });
 });
 router.get('/sign-up', function(req, res, next) {
