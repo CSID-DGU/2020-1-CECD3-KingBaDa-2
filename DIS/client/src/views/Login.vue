b6de077b2b280ce43c90c11a46a15da108d43e1a
dss1111 <dss11111@naver.com>
2020-08-12 10:33:00 +0900

    로그인 json 으로 수정

--- DIS/client/src/views/Login.vue
+++ DIS/client/src/views/Login.vue
@@ -22,12 +22,13 @@ export default {
     return {
       name: "Login",
       id: "",
-      password: "",
+      password: ""
     };
   },
   methods: {
     loginUser() {
-      axios.post("/api/login", this.id, this.password)
+      const dt = {data: {id: this.id, password:this.password}}
+      axios.post("/api/sign/login", dt)
       .then(r => console.log("r: ", JSON.stringify(r, null, 2)))
       .catch(function (error){
         console.log(error.response);
