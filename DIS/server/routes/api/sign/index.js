ca3b249adfb4fc4f47c218e3735f8e7fdee99b95
dss1111 <dss11111@naver.com>
2020-08-12 15:23:21 +0900

    검색조건을 json으로 받아서 elasticsearch에 검색하고  결과를 console에 출력

--- DIS/server/routes/api/sign/index.js
+++ DIS/server/routes/api/sign/index.js
@@ -5,10 +5,9 @@ const crypto = require('crypto');
 const request = require('request');
 
 router.post('/login', (req, res, next) => {
-    console.log(req.body.data)
     res.send({ success: true });
 });
 router.get('/sign-up', function(req, res, next) {
     console.log(req.body)
-  });
+});
 module.exports = router;
