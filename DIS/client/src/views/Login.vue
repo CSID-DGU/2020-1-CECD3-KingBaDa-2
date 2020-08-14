d1579d8b8692e178a5204dcccca598b21d7989ce
dss1111 <dss11111@naver.com>
2020-08-14 11:23:21 +0900

    원하는 데이터와 검색어를 넘기면 해당 검색결과의 원하는 데이터 부분만 출력하는 부분 작성

--- DIS/client/src/views/Login.vue
+++ DIS/client/src/views/Login.vue
@@ -27,8 +27,13 @@ export default {
   },
   methods: {
     loginUser() {
-      const dt = [{id:this.id} , {password:this.password}]
-      axios.post("/api/elastic/get", dt)
+      const dt = {
+        key:"name,value",
+        search:[
+          {name:this.id},{location1:this.password}
+        ]
+      }
+      axios.post("/api/elastic/get2", dt)
       .then(r => console.log("r: ", JSON.stringify(r, null, 2)))
       .catch(function (error){
         console.log(error.response);
--- DIS/server/routes/api/elastic/index.js
+++ DIS/server/routes/api/elastic/index.js
@@ -6,13 +6,6 @@ const elasticsearch = require('elasticsearch');
 /*data1{id:123},data2{password:123}*/ 
 router.post('/get', function(req, res, next) {
     let search = req.body;
-    // {},{}
-    /*
-    search.forEach(function(key,value){
-        console.log("hohohoho");
-        console.log(key,value);
-    });
-    */
     let client = elasticsearch.Client({
     host: '192.168.0.5:9200'
     });
@@ -34,31 +27,61 @@ router.post('/get', function(req, res, next) {
     index: 'test',
     type: '_doc',
     body: frame,
-    /*
-    body: {
-        query: {
-            bool: {
-                must:[
-                    {match:search1},
-                    {match:search2}
-                ]
-            }
-        }
-    }
-    */
     }).then(function(response) {
     let hits = response.hits.hits;//결과배열
     console.log(hits);
-    // let result = new Object();
-    //console.log(hits[0]._source);//결과 첫번째의 _source의 id 출력
-    //
-    //   파싱부분
-    //
     res.json(hits);
 
     }, function(error) {
     console.trace(error.message);
     });
 
+});
+router.post('/get2', function(req, res, next) {
+    let key = req.body.key;
+    let search = req.body.search;
+    let keysplit = key.split(',');
+
+    //console.log(key);
+    //console.log(search);
+    let client = elasticsearch.Client({
+    host: '192.168.0.5:9200'
+    });
+    let frame ={
+        query:{
+            bool:{
+                must:[
+
+                ]
+            }
+        }
+    }
+    let obj=new Object();
+    search.forEach(function(element){
+        obj.match=element;
+        frame.query.bool.must.push(obj);
+    });
+    //console.log(obj);
+    client.search({
+    index: 'test',
+    type: '_doc',
+    body: frame,
+    }).then(function(response) {
+    let hits = response.hits.hits;
+    //결과배열
+    let jsonArray=new Array();
+    let json; 
+    hits.forEach(function(element){
+        json = new Object();
+        for(let i in keysplit){
+            json[keysplit[i]] = element._source[keysplit[i]];
+        }
+        jsonArray.push(json);
+    });
+    console.log(jsonArray);
+    res.json(hits);
+    }, function(error) {
+    console.trace(error.message);
+    });
 });
 module.exports = router;
