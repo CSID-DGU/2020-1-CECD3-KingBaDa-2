82b789060341c73f85711618897f92f50b414115
dss1111 <dss11111@naver.com>
2020-08-21 09:12:25 +0900

    elastic 경로 수정

--- DIS/server/routes/api/elastic/index.js
+++ DIS/server/routes/api/elastic/index.js
@@ -5,11 +5,11 @@ const elasticsearch = require('elasticsearch');
 /*
 [{id:123},{password:123}]
 검색조건에 해당하는 모든 결과를 json으로 리턴
-*/
-router.post('/get', async function(req, res, next) {
+*/ 
+router.post('/elastic-all', async function(req, res, next) {
     let search = req.body;
     let client = elasticsearch.Client({
-    host: '192.168.0.5:9200'
+    host: '1.223.111.140:9200'
     });
     let frame ={
         size: 10000,
@@ -43,19 +43,18 @@ router.post('/get', async function(req, res, next) {
     }, function(error) {
     console.trace(error.message);
     });
-
 });
 /*
 {key:"name,value", search:[{name:this.id},{location1:this.password}]}
 key는 검색한 결과에서 가져올 부분
-search는 검색조건
+search는 검색조건 
 */
-router.post('/get2', async function(req, res, next) {
+router.post('/elastic-part', async function(req, res, next) {
     let key = req.body.key; //결과로 받고싶은 필드명
     let search = req.body.search;// 검색어
     let keysplit = key.split(',');
     let client = elasticsearch.Client({
-    host: '192.168.0.5:9200'
+    host: '1.223.111.140:9200'
     });
     let frame ={
         size: 10000,
@@ -79,7 +78,7 @@ router.post('/get2', async function(req, res, next) {
     }).then(function(response) {
     let hits = response.hits.hits;//결과배열
     let jsonArray=new Array();
-    let json;
+    let json; 
     hits.forEach(function(element){ //검색결과 순회
         json = new Object();
         for(let i in keysplit){ //key들 순회
@@ -89,6 +88,76 @@ router.post('/get2', async function(req, res, next) {
     });
     console.log(jsonArray);
     res.json(jsonArray);
+    }, function(error) {
+    console.trace(error.message);
+    });
+});
+/*
+[{name:"elc1"},{date:{"gte":"2020-01-01","lte":"2020-01-02"}}]
+배열내의 date필드를 제외한 나머지부분이 조건, date필드의 경우 기간의 범위
+*/
+router.post('/elastic-cal',async function(req, res, next) {
+    let search =req.body;
+    let client = elasticsearch.Client({
+    host: '1.223.111.140:9200'
+    });
+    let frame ={
+        size: 10000,
+        query:{
+            bool:{
+                must:[
+
+                ]
+            }
+        }
+    }
+    let obj=new Object();
+    search.forEach(function(element){
+        if(element.hasOwnProperty("date")){
+            let range = {range:{}};
+            range.range=element;
+            frame.query.bool.must.push(range);
+        }else {
+            obj.match=element;
+            frame.query.bool.must.push(obj);
+        }
+    });
+    console.log(frame.query.bool.must);
+    await client.search({
+    index: 'test',
+    type: '_doc',
+    body: frame,
+    }).then(function(response) {
+    let hits = response.hits.hits;//결과배열
+    //_source부분만 남도록 파싱//
+    let i=0;
+    let avg=0;
+    let sum=0;
+    let min=hits[0]._source.value;
+    let max=hits[0]._source.value;
+    let obj=new Object();
+    obj=[];
+    hits.forEach(function(element)
+    {
+        if(min>element._source.value){
+            min=element._source.value;
+        }
+        if(max<element._source.value){
+            max=element._source.value;
+        }
+        sum+=element._source.value;
+        i++;
+        obj.push(element._source.value);
+    });
+    avg=sum/i;
+    obj={};
+    obj.min=min;
+    obj.max=max;
+    obj.sum=sum;
+    obj.avg=avg;
+    //평균,누적,최소,최대
+    console.log(obj);
+    res.json(obj);
     }, function(error) {
     console.trace(error.message);
     });
